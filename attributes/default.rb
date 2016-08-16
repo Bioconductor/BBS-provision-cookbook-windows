c71ea97dc80c9de4138bfc29a2a53bff43a21ca1
Andrzej Oleś <andrzej.oles@gmail.com>
2016-08-16 21:51:43 +0200

    Update R download links

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,8 @@
 default['bioc_version'] = {rel: '3.3', dev: '3.4'}
 default['use_r_devel'] = {rel: false, dev: false}
 default['r_version'] = {rel: '3.3', dev: '3.3'}
-default['r_url'] = {rel: 'https://s3.amazonaws.com/bioc-windows-setup/R-devel-493-win.exe',
-   dev: 'https://s3.amazonaws.com/bioc-windows-setup/R-devel-493-win.exe'}
+default['r_url'] = {rel: 'https://cloud.r-project.org/bin/windows/base/R-3.3.1-win.exe',
+   dev: 'https://cloud.r-project.org/bin/windows/base/R-devel-win.exe'}
 default['rtools_url'] = {rel: 'https://cran.r-project.org/bin/windows/Rtools/Rtools33.exe',
    dev: 'https://cran.r-project.org/bin/windows/Rtools/Rtools34.exe'}
 default['rtools_ver_major'] = {rel: "33", dev: "34"}
